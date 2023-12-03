ec4cbae097caa982b29fb90fa94b1763403007a6
omri44 <omrikir44@gmail.com>
2023-12-03 18:37:18 +0200

    clearalltodo

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,122 +1,121 @@
-import React, { useEffect, useState } from 'react'
-import './Tasks.css'
-import { Accessibility,Trash2, FileEdit, Loader2} from 'lucide-react';
+import React, { useEffect, useState } from 'react';
+import './Tasks.css';
+import { Accessibility, Trash2, FileEdit, Loader2 } from 'lucide-react';
 import { Link } from 'react-router-dom';
 import { wait } from '../lib/utils';
 import { error, success } from '../lib/notification';
 
-
 export const Tasks = () => {
-    const [todo, setTodo] = useState('')
-    const [todos, setTodos] = useState([])
-    const [todoIndex,setTodoIndex] = useState(-1)
-    const [onClickCheck,setCnClickCheck] = useState(false)
+  const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState([]);
+  const [todoIndex, setTodoIndex] = useState(-1);
+  const [onClickCheck, setCnClickCheck] = useState(false);
 
-
-
-    useEffect(() => {
-     // console.log(localStorage.getItem('todos'))
-        const todosInStorage = JSON.parse(localStorage.getItem('todos'))
-        if(todosInStorage){
-            setTodos(todosInStorage);
-        }
-    },[todo])
-
-    
-
-
-    const handleAddTodo = async () => {
-        if(!todo) return;
-        setCnClickCheck(true)
-        await wait(1000)
-        try{
-        if(todoIndex > -1){
-            todos[todoIndex] = todo
-            setTodoIndex(-1)
-            localStorage.setItem('todos',JSON.stringify(todos))
-
-        }
-        else {
-            const allTodos=[...todos, todo]
-           // setTodos(allTodos)
-            localStorage.setItem('todos',JSON.stringify(allTodos))
-            success('task added successfully')
-
-            //setTodos([...todos, todo])/* השלוש נקודות זה כל האיברים הקודמים במערך, לוקח את כל האיברים הקודמים ומוסיף איבר חדש*/
-            
-        }
-        setTodo('')
-    }catch (err){
-        error(err?.messege)
-    }finally{
-        setCnClickCheck(false)
-
+  useEffect(() => {
+    const todosInStorage = JSON.parse(localStorage.getItem('todos'));
+    if (todosInStorage) {
+      setTodos(todosInStorage);
     }
-    }
-    const handleDeleteTodo = (index) => {
-        const newTodos=[...todos];
-        newTodos.splice(index, 1);//מקבל את המיקום וכמה למחוק
-        //newTodos.at()//לקבל את האיבר האחרון
-        setTodos(newTodos)
-        localStorage.setItem('todos',JSON.stringify(newTodos))
-    }
+  }, [todo]);
 
-    const handleUpdateTodo = (index) => {
-        setTodo(todos[index])
-        setTodoIndex(index)
+  const handleAddTodo = async () => {
+    if (!todo) return;
+    setCnClickCheck(true);
+    await wait(1000);
+    try {
+      if (todoIndex > -1) {
+        todos[todoIndex] = todo;
+        setTodoIndex(-1);
+        localStorage.setItem('todos', JSON.stringify(todos));
+      } else {
+        const allTodos = [...todos, todo];
+        localStorage.setItem('todos', JSON.stringify(allTodos));
+        success('Task added successfully');
+      }
+      setTodo('');
+    } catch (err) {
+      error(err?.message);
+    } finally {
+      setCnClickCheck(false);
     }
+  };
 
-    return (
+  const handleDeleteTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+    localStorage.setItem('todos', JSON.stringify(newTodos));
+  };
+
+  const handleUpdateTodo = (index) => {
+    setTodo(todos[index]);
+    setTodoIndex(index);
+  };
+
+  const handleClearAllTodo = () => {
+    if(window.confirm("Are you sure you want to delete all tasks?"))
+    setTodos([]);
+    localStorage.removeItem('todos');
+  };
+
+  return (
     <div className='container'>
-        <div>
-            <input 
-                onChange={({target})=>setTodo(target.value)}
-                value={todo}
-                className='task-input'
-                disabled={onClickCheck} 
-                placeholder='Add a new task...' 
-                onKeyDown={(e) => {
-                    if(e.key === 'Enter'){
-                        handleAddTodo()
-                    }
-                }}
-            />
-            {!onClickCheck ? (
-            <Accessibility 
-                onClick={handleAddTodo}
-                className='task-icon' 
-                style={{cursor:todo ? 'pointer':'not-allowed'}}
-            />
-            ):(
-                <Accessibility className='task-icon loading'/>
-            )}
-        </div>
-        <div className='todos-wrapper'>
-            {todos?.map((todo, index)=>(
-               <div className='todo-item' key={index}>
-                    <Link to={`/todo/${index}`} className='todo-link'>
-                        <span style={{opacity: todoIndex === index ? 0.5 : 1}}>
-                          {index+1}.{todo}
-                        </span>
-                    </Link>
-
-                    <div>
-                        <FileEdit 
-                        onClick={()=>handleUpdateTodo(index)}
-                        className='pencil-icon' />
-                        <Trash2 
-                        onClick={()=>handleDeleteTodo(index)}
-                        className='trash-icon'
-                        />
-                    </div>
-                </div>
-            ))}
-            <div className='todo-item'>
-                {(todoIndex === -1 && onClickCheck) && <span style={{opacity: 0.5}}>
-                    {todos.length + 1}.{todo}
-                </span>}
+      <div>
+        <input
+          onChange={({ target }) => setTodo(target.value)}
+          value={todo}
+          className='task-input'
+          disabled={onClickCheck}
+          placeholder='Add a new task...'
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              handleAddTodo();
+            }
+          }}
+        />
+        {!onClickCheck ? (
+          <Accessibility
+            onClick={handleAddTodo}
+            className='task-icon'
+            style={{ cursor: todo ? 'pointer' : 'not-allowed' }}
+          />
+        ) : (
+          <Accessibility className='task-icon loading' />
+        )}
+      </div>
+      <div className='todos-wrapper'>
+        {todos?.map((todo, index) => (
+          <div className='todo-item' key={index}>
+            <Link to={`/todo/${index}`} className='todo-link'>
+              <span style={{ opacity: todoIndex === index ? 0.5 : 1 }}>
+                {index + 1}.{todo}
+              </span>
+            </Link>
+            <div>
+              <FileEdit
+                onClick={() => handleUpdateTodo(index)}
+                className='pencil-icon'
+              />
+              <Trash2
+                onClick={() => handleDeleteTodo(index)}
+                className='trash-icon'
+              />
             </div>
+          </div>
+        ))}
+        <div className='todo-item'>
+          {(todoIndex === -1 && onClickCheck) && (
+            <span style={{ opacity: 0.5 }}>{todos.length + 1}.{todo}</span>
+          )}
         </div>
+      </div>
+      <div>
+        {todos.length > 0 && (
+          <button onClick={handleClearAllTodo} className='clear-all-button'>
+            Clear All
+          </button>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
